8ec7dc1eb8a388c337023a31b2583ca84f80c043
kine09 <nguyenkien.hn09@gmail.com>
2021-12-19 15:26:19 +0700

    update

--- asset/main.js
+++ asset/main.js
@@ -1,22 +1,21 @@
 // header
 {
-    // on/off menunavbar
+  // on/off menunavbar
 
-    var headerNavBtn = document.querySelector('.header__box2-navBtn')
-    var headerBox1 = document.querySelector('.header__box1')
+  var headerNavBtn = document.querySelector(".header__box2-navBtn");
+  var headerBox1 = document.querySelector(".header__box1");
 
-    headerNavBtn.addEventListener('click', ()=>{
-        headerBox1.classList.toggle('hide')
-        headerBox1.classList.toggle('off')
-    })
+  headerNavBtn.addEventListener("click", () => {
+    headerBox1.classList.toggle("hiden");
+  });
 
-    // hide / unhide search bar
-    var searchBtn = document.querySelector('.search__bar-btn')
-    var searchInput = document.querySelector('.search__bar-input')
+  // hide / unhide search bar
+  var searchBtn = document.querySelector(".search__bar-btn");
+  var searchInput = document.querySelector(".search__bar-input");
 
-    searchBtn.addEventListener('click', ()=>{
-        searchInput.classList.toggle('off')
-    })
+  searchBtn.addEventListener("click", () => {
+    searchInput.classList.toggle("off");
+  });
 }
 
 // form dang ki/ dang nhap
@@ -32,9 +31,9 @@
         element = element.parentElement;
       }
     }
-  
+
     var selectorRules = {};
-  
+
     // Hàm thực hiện validate
     function Validate(inputElement, rule) {
       var errorrElement = getParent(
@@ -42,10 +41,10 @@
         options.formGroup
       ).querySelector(options.errorSelector);
       var errorrMessage;
-  
+
       // lấy qua các rule của selectort
       var rules = selectorRules[rule.selector];
-  
+
       // lặp qua tưng rules của selector
       // nếu có lỗi dừng kiểm tra
       for (var i = 0; i < rules.length; ++i) {
@@ -61,7 +60,7 @@
         }
         if (errorrMessage) break;
       }
-  
+
       if (errorrMessage) {
         errorrElement.innerText = errorrMessage;
         inputElement.parentElement.classList.add("valid");
@@ -71,16 +70,16 @@
       }
       return !errorrMessage;
     }
-  
+
     //  Lấy element của form cần validate
     var formElement = document.querySelector(options.form);
     if (formElement) {
       // khi submit form
       formElement.onsubmit = function (e) {
         e.preventDefault();
-  
+
         var isFormValid = true;
-  
+
         // lặp qua từng rules và validate
         options.rules.forEach(function (rule) {
           var inputElement = formElement.querySelector(rule.selector);
@@ -89,9 +88,11 @@
             isFormValid = false;
           }
         });
-  
-        var enableInputs = formElement.querySelectorAll("[name]:not([disable])");
-  
+
+        var enableInputs = formElement.querySelectorAll(
+          "[name]:not([disable])"
+        );
+
         var formValues = Array.from(enableInputs).reduce(function (
           values,
           input
@@ -99,7 +100,7 @@
           return (values[input.name] = input.value) && values;
         },
         {});
-  
+
         if (isFormValid) {
           if (typeof options.onsubmit === "function") {
             var enableInputs = formElement.querySelectorAll(
@@ -127,15 +128,15 @@
         } else {
           selectorRules[rule.selector] = [rule.test];
         }
-  
+
         var inputElements = formElement.querySelectorAll(rule.selector);
-  
+
         Array.from(inputElements).forEach(function (inputElement) {
           // xử lý khi blur khỏi input
           inputElement.onblur = function () {
             Validate(inputElement, rule);
           };
-  
+
           // xử lý khi nhập vào input
           inputElement.oninput = function () {
             var errorrElement = inputElement.parentElement.querySelector(
@@ -148,7 +149,7 @@
       });
     }
   }
-  
+
   // Định nghĩa Hàm
   // nguyên tắc các rules
   // 1. khi có lỗi => message lỗi
@@ -161,7 +162,7 @@
       },
     };
   };
-  
+
   Validator.isEmail = function (selector, message) {
     return {
       selector: selector,
@@ -173,7 +174,7 @@
       },
     };
   };
-  
+
   Validator.minLength = function (selector, min, message) {
     return {
       selector: selector,
@@ -184,7 +185,7 @@
       },
     };
   };
-  
+
   Validator.isConfirm = function (selector, getConfirmValue, message) {
     return {
       selector: selector,
@@ -199,132 +200,136 @@
 
 // chuyen tab dang ki/ dang nhap
 {
-var changeForms = document.querySelectorAll('.form__title-sub')
+  var changeForms = document.querySelectorAll(".form__title-sub");
 
-var modalRegisterForm = document.querySelector('.modal__register')
-var modalLoginForm = document.querySelector('.modal__login')
+  var modalRegisterForm = document.querySelector(".modal__register");
+  var modalLoginForm = document.querySelector(".modal__login");
 
-var headerRegisterBtns = document.querySelectorAll('.header__register')
-var headerloginBtns = document.querySelectorAll('.header__login')
+  var headerRegisterBtns = document.querySelectorAll(".header__register");
+  var headerloginBtns = document.querySelectorAll(".header__login");
 
-for(var changeForm of changeForms) {
-  changeForm.addEventListener('click', ()=>{
-    modalRegisterForm.classList.toggle('hide')
-    modalLoginForm.classList.toggle('hide')
-
-  })
-}
+  for (var changeForm of changeForms) {
+    changeForm.addEventListener("click", () => {
+      modalRegisterForm.classList.toggle("hide");
+      modalLoginForm.classList.toggle("hide");
+    });
+  }
   //  on/ off modal
-var modal = document.querySelector('.modal')
-var cancelModalbtns = document.querySelectorAll('.form__cancel')
+  var modal = document.querySelector(".modal");
+  var cancelModalbtns = document.querySelectorAll(".form__cancel");
 
+  for (var headerRegisterBtn of headerRegisterBtns) {
+    headerRegisterBtn.addEventListener("click", () => {
+      modal.classList.add("unhide");
+      modal.classList.remove("hide");
+      modalRegisterForm.classList.remove("hide");
+      modalLoginForm.classList.add("hide");
+    });
+  }
+  for (var headerloginBtn of headerloginBtns) {
+    headerloginBtn.addEventListener("click", () => {
+      modal.classList.add("unhide");
+      modal.classList.remove("hide");
+      modalRegisterForm.classList.add("hide");
+      modalLoginForm.classList.remove("hide");
+    });
+  }
 
-for(var headerRegisterBtn of headerRegisterBtns) {
-  headerRegisterBtn.addEventListener('click',()=>{
-    modal.classList.add('unhide')
-    modal.classList.remove('hide')
-    modalRegisterForm.classList.remove('hide')
-    modalLoginForm.classList.add('hide')
-  })
-}
-for(var headerloginBtn of headerloginBtns) {
-headerloginBtn.addEventListener('click',()=>{
-  modal.classList.add('unhide')
-  modal.classList.remove('hide')
-  modalRegisterForm.classList.add('hide')
-  modalLoginForm.classList.remove('hide')
-})
-}
+  for (var cancelModalbtn of cancelModalbtns) {
+    cancelModalbtn.addEventListener("click", () => {
+      modal.classList.remove("unhide");
+      modal.classList.add("hide");
+    });
+  }
 
-for(var cancelModalbtn of cancelModalbtns) {
-  cancelModalbtn.addEventListener('click', ()=>{
-    modal.classList.remove('unhide')
-    modal.classList.add('hide')
-  })
-}
+  modal.addEventListener("click", () => {
+    modal.classList.remove("unhide");
+    modal.classList.add("hide");
+  });
 
-modal.addEventListener('click', ()=>{
-  modal.classList.remove('unhide')
-  modal.classList.add('hide')
-})
-
-document.querySelector('.modal-wapper').addEventListener('click',(e)=>{
-  e.stopPropagation()
-})
-
+  document.querySelector(".modal-wapper").addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
 }
 // category
 {
-  var categoryItems = document.querySelectorAll('.category__item')
-  for(var categoryItem of categoryItems) {
-    categoryItem.addEventListener('click', function(){
-      document.querySelector('.category__item.category__item--active').classList.remove('category__item--active')
-      this.classList.add('category__item--active')
-    })
+  var categoryItems = document.querySelectorAll(".category__item");
+  for (var categoryItem of categoryItems) {
+    categoryItem.addEventListener("click", function () {
+      document
+        .querySelector(".category__item.category__item--active")
+        .classList.remove("category__item--active");
+      this.classList.add("category__item--active");
+    });
   }
 }
 
 // sắp sếp theo loại sản phẩm
 
 {
-  var categoryItemAll = document.querySelector('.category__item[type="all"]')
-  var categoryItemClothes = document.querySelector('.category__item[type="clothes"]')
-  var categoryItemWinter = document.querySelector('.category__item[type="winter"]')
-  var categoryItemother = document.querySelector('.category__item[type="other"]')
-  
-  var itemAlls = document.querySelectorAll('.product__item')
-  var itemClothess = document.querySelectorAll('.product__item[rule="clothes"]')
-  var itemWinters = document.querySelectorAll('.product__item[rule="winter"]')
-  var itemOthers = document.querySelectorAll('.product__item[rule="other"]')
+  var categoryItemAll = document.querySelector('.category__item[type="all"]');
+  var categoryItemClothes = document.querySelector(
+    '.category__item[type="clothes"]'
+  );
+  var categoryItemWinter = document.querySelector(
+    '.category__item[type="winter"]'
+  );
+  var categoryItemother = document.querySelector(
+    '.category__item[type="other"]'
+  );
 
+  var itemAlls = document.querySelectorAll(".product__item");
+  var itemClothess = document.querySelectorAll(
+    '.product__item[rule="clothes"]'
+  );
+  var itemWinters = document.querySelectorAll('.product__item[rule="winter"]');
+  var itemOthers = document.querySelectorAll('.product__item[rule="other"]');
 
+  categoryItemAll.addEventListener("click", () => {
+    for (var itemAll of itemAlls) {
+      itemAll.parentElement.classList.remove("hide");
+    }
+  });
 
-  categoryItemAll.addEventListener('click', ()=>{
-    for(var itemAll of itemAlls){
-      itemAll.parentElement.classList.remove('hide')
+  categoryItemClothes.addEventListener("click", () => {
+    for (var itemAll of itemAlls) {
+      itemAll.parentElement.classList.add("hide");
     }
-  })
+    for (var itemClothes of itemClothess) {
+      itemClothes.parentElement.classList.remove("hide");
+    }
+  });
 
-  categoryItemClothes.addEventListener('click', ()=>{
-    for(var itemAll of itemAlls){
-      itemAll.parentElement.classList.add('hide')
+  categoryItemWinter.addEventListener("click", () => {
+    for (var itemAll of itemAlls) {
+      itemAll.parentElement.classList.add("hide");
     }
-    for(var itemClothes of itemClothess){
-      itemClothes.parentElement.classList.remove('hide')
+    for (var itemWinter of itemWinters) {
+      itemWinter.parentElement.classList.remove("hide");
     }
-  })
+  });
 
-  categoryItemWinter.addEventListener('click', ()=>{
-    for(var itemAll of itemAlls){
-      itemAll.parentElement.classList.add('hide')
+  categoryItemother.addEventListener("click", () => {
+    for (var itemAll of itemAlls) {
+      itemAll.parentElement.classList.add("hide");
     }
-    for(var itemWinter of itemWinters){
-      itemWinter.parentElement.classList.remove('hide')
+    for (var itemOther of itemOthers) {
+      itemOther.parentElement.classList.remove("hide");
     }
-  })
-
-  categoryItemother.addEventListener('click', ()=>{
-    for(var itemAll of itemAlls){
-      itemAll.parentElement.classList.add('hide')
-    }
-    for(var itemOther of itemOthers){
-      itemOther.parentElement.classList.remove('hide')
-    }
-  })
-
+  });
 }
 
 // sắp xếp theo trending
 
 {
-  var trendingBtns = document.querySelectorAll('.product__navbar-sort-btn')
+  var trendingBtns = document.querySelectorAll(".product__navbar-sort-btn");
 
-  for(var trendingBtn of trendingBtns){
-    trendingBtn.addEventListener('click', function(){
-      document.querySelector('.product__navbar-sort-btn.btn--active').classList.remove('btn--active')
-      this.classList.add('btn--active')
-    })
+  for (var trendingBtn of trendingBtns) {
+    trendingBtn.addEventListener("click", function () {
+      document
+        .querySelector(".product__navbar-sort-btn.btn--active")
+        .classList.remove("btn--active");
+      this.classList.add("btn--active");
+    });
   }
 }
-
-
